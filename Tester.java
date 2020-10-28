86140ef7580965177150a832938563998eff5345
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-27 21:00:52 -0400

    Test successful cases for desposit and withdraw

--- Tester.java
+++ Tester.java
@@ -9,5 +9,15 @@ public class Tester {
 
         // Test that setPassword doesn't crash
         mine.setPassword("better_password");
+
+        // Test successful cases for deposit and withdraw
+        System.out.println(mine.deposit(80.0)); // true
+        System.out.println(mine.deposit(0.0)); // true
+        System.out.println(mine.toString()); // 1134978\t80.0
+        System.out.println(mine.withdraw(40.0)); // true
+        System.out.println(mine.withdraw(0.0)); // true
+        System.out.println(mine.toString()); // 1134978\t40.0
+        System.out.println(mine.withdraw(40.0)); // true
+        System.out.println(mine.toString()); // 1134978\t0.0
     }
 }
