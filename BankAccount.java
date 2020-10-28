3daee4479fa96ae8f5a8f6153ef477da3eb90f24
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-27 20:14:50 -0400

    Create get method for accountID

--- BankAccount.java
+++ BankAccount.java
@@ -8,5 +8,8 @@ public class BankAccount {
         this.accountID = accountID;
         this.password = password;
     }
-    
+
+    public int getAccountID() {
+        return accountID;
+    }
 }
