4250bdffa633c6340c32e87c8fb5617ca58708fe
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-27 20:52:29 -0400

    Test getters and setPassword()

--- Tester.java
+++ Tester.java
@@ -1,3 +1,13 @@
 public class Tester {
-    
+    public static void main(String[] args) {
+        BankAccount mine = new BankAccount(1134978, "password");
+
+        // Test that information getters work at all
+        System.out.println(mine.getBalance()); // 0
+        System.out.println(mine.getAccountID()); // 1134978
+        System.out.println(mine.toString()); // 1134978\t0.0
+
+        // Test that setPassword doesn't crash
+        mine.setPassword("better_password");
+    }
 }
