611e610fa9ea02152dcaa10db86faf25babb7708
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-27 20:14:02 -0400

    Create constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,11 @@ public class BankAccount {
     private double balance;
     private int accountID;
     private String password;
+
+    public BankAccount(int accountID, String password) {
+        balance = 0;
+        this.accountID = accountID;
+        this.password = password;
+    }
+    
 }
